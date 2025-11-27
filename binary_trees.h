3e8d772f9fc3480dfbbfade118e11cfab50426ed
Ayse <ayse@example.com>
2025-11-27 19:04:57 +0000

    Implement binary_tree_insert_left function and update header

--- binary_trees.h
+++ binary_trees.h
@@ -1,12 +1,9 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
-
-#include <stddef.h>
-#include <stdlib.h>
-#include <stdio.h>
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -20,7 +17,9 @@ typedef struct binary_tree_s
     struct binary_tree_s *right;
 } binary_tree_t;
 
-void binary_tree_print(const binary_tree_t *tree);
+/* Function prototypes */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
+void binary_tree_print(const binary_tree_t *tree);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
