6d61c43cb27718ced91989ebac3d569406d0abae
Ayse <ayse@example.com>
2025-11-28 10:14:56 +0000

    Update binary tree node and print functions

--- binary_tree_print.c
+++ binary_tree_print.c
@@ -1,99 +1,34 @@
-#include <stdio.h>
+#include <stddef.h>
 #include <stdlib.h>
+#include <stdio.h>
 #include "binary_trees.h"
 
-/**
- * binary_tree_print - Simple binary tree printer
- * @tree: Pointer to the root node of the tree to print
- */
+/* Original binary_tree_print function */
 void binary_tree_print(const binary_tree_t *tree)
 {
-    if (tree == NULL)
-    {
-        printf("(null)\n");
+    char *s;
+    int t;
+
+    if (!tree)
         return;
-    }
-    
-    printf("       .-------(%03d)-------.\n", tree->n);
-    
-    if (tree->left != NULL)
+    s = "()";
+    t = tree->n;
+    printf("(%d)", t);
+    if (tree->left || tree->right)
     {
-        if (tree->left->left != NULL || tree->left->right != NULL)
+        if (tree->left)
         {
-            printf("  .--(%03d)--.         ", tree->left->n);
+            printf(" -> ");
+            binary_tree_print(tree->left);
         }
         else
+            printf("%s", s);
+        if (tree->right)
         {
-            printf("  .--(%03d)              ", tree->left->n);
-        }
-    }
-    else
-    {
-        printf("                      ");
-    }
-    
-    if (tree->right != NULL)
-    {
-        if (tree->right->left != NULL || tree->right->right != NULL)
-        {
-            printf(".--(%03d)--.\n", tree->right->n);
+            printf(" -> ");
+            binary_tree_print(tree->right);
         }
         else
-        {
-            printf("(%03d)\n", tree->right->n);
-        }
+            printf("%s", s);
     }
-    else
-    {
-        printf("\n");
-    }
-    
-    /* Print the second level */
-    if (tree->left != NULL)
-    {
-        if (tree->left->left != NULL)
-        {
-            printf("(%03d)     ", tree->left->left->n);
-        }
-        else
-        {
-            printf("(nil)     ");
-        }
-        
-        if (tree->left->right != NULL)
-        {
-            printf("(%03d)     ", tree->left->right->n);
-        }
-        else
-        {
-            printf("(nil)     ");
-        }
-    }
-    else
-    {
-        printf("                ");
-    }
-    
-    if (tree->right != NULL)
-    {
-        if (tree->right->left != NULL)
-        {
-            printf("(%03d)     ", tree->right->left->n);
-        }
-        else
-        {
-            printf("(nil)     ");
-        }
-        
-        if (tree->right->right != NULL)
-        {
-            printf("(%03d)", tree->right->right->n);
-        }
-        else
-        {
-            printf("(nil)");
-        }
-    }
-    
-    printf("\n");
 }
