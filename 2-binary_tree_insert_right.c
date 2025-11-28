60aafeeb042d630c9b3d196f0fad10e48e22a934
Ayse <ayse@example.com>
2025-11-28 11:06:14 +0000

    Complete binary_tree_insert_right implementation

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,26 +1,32 @@
 #include <stddef.h>
 #include <stdlib.h>
 
-/* Basic binary tree structure definition */
-typedef struct binary_tree_s binary_tree_t;
-
+/**
+ * struct binary_tree_s - Binary tree node structure
+ * @n: Integer stored in the node
+ * @parent: Pointer to the parent node
+ * @left: Pointer to the left child node
+ * @right: Pointer to the right child node
+ */
 struct binary_tree_s
 {
-    int n;
-    binary_tree_t *parent;
-    binary_tree_t *left;
-    binary_tree_t *right;
+	int n;
+	struct binary_tree_s *parent;
+	struct binary_tree_s *left;
+	struct binary_tree_s *right;
 };
+
+typedef struct binary_tree_s binary_tree_t;
 
 /* External function prototype */
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 /**
- * binary_tree_insert_right - Inserts a node as the right-child of another node
- * @parent: Pointer to the node to insert the right-child in
- * @value: Value to store in the new node
+ * binary_tree_insert_right - Inserts node as right-child of another node
+ * @parent: Pointer to node to insert right-child in
+ * @value: Value to store in new node
  *
- * Return: Pointer to the created node, or NULL on failure
+ * Return: Pointer to created node, or NULL on failure
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
