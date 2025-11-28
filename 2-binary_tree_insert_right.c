55b6620f03f27e7169a727bbb51ce03cf67c2e04
Ayse <ayse@example.com>
2025-11-28 11:02:09 +0000

    Fix binary_tree_insert_right - add manual struct and prototype

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,6 +1,19 @@
 #include <stddef.h>
 #include <stdlib.h>
-#include "binary_trees.h"
+
+/* Basic binary tree structure definition */
+typedef struct binary_tree_s binary_tree_t;
+
+struct binary_tree_s
+{
+    int n;
+    binary_tree_t *parent;
+    binary_tree_t *left;
+    binary_tree_t *right;
+};
+
+/* External function prototype */
+binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 
 /**
  * binary_tree_insert_right - Inserts a node as the right-child of another node
