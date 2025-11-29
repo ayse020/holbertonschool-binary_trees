676bc8289f6b568964583cc83db6755d98366317
Ayse <ayse@example.com>
2025-11-29 11:23:42 +0000

    Add binary_tree_is_leaf function and update header

--- binary_trees.h
+++ binary_trees.h
@@ -1,10 +1,11 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
 
 /**
  * struct binary_tree_s - Binary tree node
+ *
  * @n: Integer stored in the node
  * @parent: Pointer to the parent node
  * @left: Pointer to the left child node
@@ -12,17 +13,15 @@
  */
 typedef struct binary_tree_s
 {
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
+    int n;
+    struct binary_tree_s *parent;
+    struct binary_tree_s *left;
+    struct binary_tree_s *right;
 } binary_tree_t;
 
-/* Function prototypes */
+void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-void binary_tree_delete(binary_tree_t *tree);
-void binary_tree_print(const binary_tree_t *tree);
+int binary_tree_is_leaf(const binary_tree_t *node);  /* BU SƏTRİ ƏLAVƏ EDİN */
 
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
