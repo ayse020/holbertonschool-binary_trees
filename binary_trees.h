7d1d376f1ff51ad2046894919118eb2463fab94f
Ayse <ayse@example.com>
2025-11-29 15:20:59 +0000

    Add binary_tree_nodes function to count nodes with at least 1 child

--- binary_trees.h
+++ binary_trees.h
@@ -24,7 +24,9 @@ typedef struct binary_tree_s binary_tree_t;
 /* Function prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
+binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
+void binary_tree_delete(binary_tree_t *tree);
 int binary_tree_is_leaf(const binary_tree_t *node);
 int binary_tree_is_root(const binary_tree_t *node);
 void binary_tree_preorder(const binary_tree_t *tree, void (*func)(int));
@@ -34,5 +36,6 @@ size_t binary_tree_height(const binary_tree_t *tree);
 size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
+size_t binary_tree_nodes(const binary_tree_t *tree);
 
 #endif /* BINARY_TREES_H */
