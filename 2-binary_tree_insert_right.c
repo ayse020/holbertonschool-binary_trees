7fd2e57f94340c05121999eda6489f8fe27ecbfa
Ayse <ayse@example.com>
2025-11-29 10:45:53 +0000

    Fix: Remove struct definition from .c file - Betty compliance

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,32 +1,12 @@
-#include <stddef.h>
+#include "binary_trees.h"
 #include <stdlib.h>
 
 /**
- * struct binary_tree_s - Binary tree node structure
- * @n: Integer stored in the node
- * @parent: Pointer to the parent node
- * @left: Pointer to the left child node
- * @right: Pointer to the right child node
- */
-struct binary_tree_s
-{
-	int n;
-	struct binary_tree_s *parent;
-	struct binary_tree_s *left;
-	struct binary_tree_s *right;
-};
-
-typedef struct binary_tree_s binary_tree_t;
-
-/* External function prototype */
-binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
-
-/**
- * binary_tree_insert_right - Inserts node as right-child of another node
- * @parent: Pointer to node to insert right-child in
- * @value: Value to store in new node
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
+ * @parent: Pointer to the node to insert the right-child in
+ * @value: Value to store in the new node
  *
- * Return: Pointer to created node, or NULL on failure
+ * Return: Pointer to the created node, or NULL on failure or if parent is NULL
  */
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
