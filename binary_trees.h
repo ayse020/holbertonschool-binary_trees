ef063bda1db086cc1dcf4bfb04cdcf882f2edb31
Ayse <ayse@example.com>
2025-11-29 11:37:53 +0000

    Add binary_tree_is_root function

--- binary_trees.h
+++ binary_trees.h
@@ -1,5 +1,5 @@
-#ifndef _BINARY_TREES_H_
-#define _BINARY_TREES_H_
+#ifndef BINARY_TREES_H
+#define BINARY_TREES_H
 
 #include <stddef.h>
 
@@ -11,17 +11,21 @@
  * @left: Pointer to the left child node
  * @right: Pointer to the right child node
  */
-typedef struct binary_tree_s
+struct binary_tree_s
 {
     int n;
     struct binary_tree_s *parent;
     struct binary_tree_s *left;
     struct binary_tree_s *right;
-} binary_tree_t;
+};
 
+typedef struct binary_tree_s binary_tree_t;
+
+/* Function prototypes */
 void binary_tree_print(const binary_tree_t *);
 binary_tree_t *binary_tree_node(binary_tree_t *parent, int value);
 binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value);
-int binary_tree_is_leaf(const binary_tree_t *node);  /* BU SƏTRİ ƏLAVƏ EDİN */
+int binary_tree_is_leaf(const binary_tree_t *node);
+int binary_tree_is_root(const binary_tree_t *node);
 
-#endif /* _BINARY_TREES_H_ */
+#endif /* BINARY_TREES_H */
