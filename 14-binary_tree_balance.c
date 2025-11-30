eb060a7e979f44562855422ec61c57f792ee8636
Ayse <ayse@example.com>
2025-11-30 14:43:12 +0000

    Add binary_tree_balance function

---
+++ 14-binary_tree_balance.c
@@ -0,0 +1,42 @@
+#include "binary_trees.h"
+
+/**
+ * calculate_height - Calculates height for balance factor
+ * @tree: Pointer to the root node
+ *
+ * Return: Height of the tree
+ */
+int calculate_height(const binary_tree_t *tree)
+{
+    int left_h, right_h;
+
+    if (tree == NULL)
+        return (0);
+
+    left_h = calculate_height(tree->left);
+    right_h = calculate_height(tree->right);
+
+    if (left_h > right_h)
+        return (left_h + 1);
+    else
+        return (right_h + 1);
+}
+
+/**
+ * binary_tree_balance - Measures the balance factor of a binary tree
+ * @tree: Pointer to the root node of the tree
+ *
+ * Return: Balance factor
+ */
+int binary_tree_balance(const binary_tree_t *tree)
+{
+    int left_height, right_height;
+
+    if (tree == NULL)
+        return (0);
+
+    left_height = calculate_height(tree->left);
+    right_height = calculate_height(tree->right);
+
+    return (left_height - right_height);
+}
--- binary_trees.h
+++ binary_trees.h
@@ -1,7 +1,8 @@
-#ifndef BINARY_TREES_H
-#define BINARY_TREES_H
+#ifndef _BINARY_TREES_H_
+#define _BINARY_TREES_H_
 
 #include <stddef.h>
+#include <stdlib.h>
 
 /**
  * struct binary_tree_s - Binary tree node
@@ -37,5 +38,6 @@ size_t binary_tree_depth(const binary_tree_t *tree);
 size_t binary_tree_size(const binary_tree_t *tree);
 size_t binary_tree_leaves(const binary_tree_t *tree);
 size_t binary_tree_nodes(const binary_tree_t *tree);
+int binary_tree_balance(const binary_tree_t *tree);
 
-#endif /* BINARY_TREES_H */
+#endif /* _BINARY_TREES_H_ */
